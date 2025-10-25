398ded240c68e0aef1017737049a65e49abf0b32
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2025-10-25 18:01:20 +0300

    started cleanup, removed init-db from docker image and repository file system

--- init-db/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# init-db.sh - Database initialization script
-
-set -e
-
-# This script runs when the PostgreSQL container is first created
-# Add any database initialization commands here
-
-echo "Database initialization complete"
