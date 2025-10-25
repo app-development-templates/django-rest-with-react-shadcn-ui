f9860a689e3e13402d77675bc6a634392f0c405b
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2025-10-25 19:47:04 +0300

    Refactor Dockerfiles to set VITE_API_URL environment variable and remove unused components; delete obsolete test and production check files

--- react-shadcn-ui/src/components/Layout.jsx
+++ react-shadcn-ui/src/components/Layout.jsx
@@ -1,11 +1,9 @@
 import Navigation from "./Navigation";
-import TokenExpirationWarning from "./TokenExpirationWarning";
 
 function Layout({ children }) {
     return (
         <div className="min-h-screen bg-gray-50">
             <Navigation />
-            <TokenExpirationWarning />
             <main>{children}</main>
         </div>
     );
